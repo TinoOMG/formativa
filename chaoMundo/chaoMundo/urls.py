037d066a6fbd625bfc2c51075ae317026e540dff
TinoOMG <graeser98@gmail.com>
2022-09-15 10:43:17 -0300

    app2 lista

--- chaoMundo/chaoMundo/urls.py
+++ chaoMundo/chaoMundo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from secondtApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/nada', views.display),
+    path('/tmp', views.display2),
 ]
